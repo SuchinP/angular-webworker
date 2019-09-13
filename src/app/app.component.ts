33941745af09a503daddc6f539dc8730a3cd4f77
SuchinP <suchin1591@gmail.com>
2019-09-13 20:07:37 +0530

    2 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,48 @@ export class AppComponent  {
   data={};
   constructor() { 
 
-     this.dashboardPermissionChart=[{id:1,name:'Suchin'},{id:1,name:'Suchin'},{id:1,name:'Suchin'},{id:1,name:'Suchin'},{id:1,name:'Suchin'},{id:1,name:'Suchin'},{id:1,name:'Suchin'},{id:1,name:'Suchin'},{id:1,name:'Suchin'},{id:1,name:'Suchin'},{id:1,name:'Suchin'},{id:1,name:'Suchin'},{id:1,name:'Suchin'}];
+    //  this.dashboardPermissionChart=[{id:1,name:'Suchin'},{id:1,name:'Suchin'},{id:1,name:'Suchin'},{id:1,name:'Suchin'},{id:1,name:'Suchin'},{id:1,name:'Suchin'},{id:1,name:'Suchin'},{id:1,name:'Suchin'},{id:1,name:'Suchin'},{id:1,name:'Suchin'},{id:1,name:'Suchin'},{id:1,name:'Suchin'},{id:1,name:'Suchin'}];
+ 
   }
+
+  
+  startWebWorker() {
+    this.dashboardPermissionChart=[{id:1,name:'Suchin'},{id:1,name:'Suchin'},{id:1,name:'Suchin'},{id:1,name:'Suchin'},{id:1,name:'Suchin'},{id:1,name:'Suchin'},{id:1,name:'Suchin'},{id:1,name:'Suchin'},{id:1,name:'Suchin'},{id:1,name:'Suchin'},{id:1,name:'Suchin'},{id:1,name:'Suchin'},{id:1,name:'Suchin'}];
+    if (typeof Worker !== 'undefined') {
+      debugger;
+      // Create a new
+      const worker = new Worker('./web-worker.worker', { type: 'module' });
+      worker.onmessage = ({ data }) => {
+        debugger;
+        // console.log(`page got message: ${data}`);
+        this.loading = false; // Hide loading widget
+      };
+      this.loading = true; // Display loading widget
+      worker.postMessage(this.dashboardPermissionChart);
+    } else {
+      debugger;
+      // Web Workers are not supported in this environment.
+      // You should add a fallback so that your program still executes correctly.
+    }
+  }
+
+  startMain() {
+
+    // Does not display, because Main thread is blocked.
+    this.loading = true;
+
+    console.log('Data Size: ' + this.dataSize);
+    console.time('Main')
+    let val = 'a';
+    for (let k = 0; k <= 10; k++) {
+      val = 'a';
+      for (let i = 0; i <= this.dataSize; i++) {
+        val += 'a';
+      }
+    }
+    console.timeEnd('Main');
+    this.loading = false;
+
+  }
+
 }
